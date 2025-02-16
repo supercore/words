c342951c489dc152a35866a66e8ad36b3b1374d3
supercore <jianghuawei@icloud.com>
2025-02-16 19:00:48 +0800

    review order; 521words

--- gears.py
+++ gears.py
@@ -8,18 +8,21 @@ filepath = "/Volumes/hynixp41/Users/701/words/flashcards.json"
 # Read the JSON file
 with open(filepath, 'r') as file:
     flashcards = json.load(file)
-flashcards_sorted = sorted(flashcards, key=lambda x: x["next_review"])
+
+# Sort flashcards by repetitions first, then by next_review
+flashcards_sorted = sorted(flashcards, key=lambda x: (x["repetitions"], x["next_review"]))
 
 # Create a table
 table = PrettyTable()
-table.field_names = ["Question", "Next Review"]
+table.field_names = ["Order", "Question", "Next Review", "Repetitions"]
 
 # Populate the table with data
-for flashcard in flashcards_sorted:
+for index, flashcard in enumerate(flashcards_sorted, start=1):
     question = flashcard["question"]
     next_review_timestamp = flashcard["next_review"]
     next_review_date = datetime.datetime.fromtimestamp(next_review_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-    table.add_row([question, next_review_date])
+    repetitions = flashcard["repetitions"]
+    table.add_row([index, question, next_review_date, repetitions])
 
 # Print the table
 print(table)
